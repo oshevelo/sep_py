f18fef1fb3dba9598a11b63051d514925adf4b14
I.Galytska <iryna.galytska@sanoma.com>
2019-09-26 14:25:03 +0300

    make tests general

--- home_work_3/calculations/date_calculator.py
+++ home_work_3/calculations/date_calculator.py
@@ -4,17 +4,20 @@ from dateutil.parser import parse
 from dateutil.relativedelta import relativedelta
 
 
-def cal_date_incr_from_today(**kwargs):
-    TODAY = date.today()
-    r = TODAY + relativedelta(**kwargs)
+def cal_date_incr_from_today(today=None, **kwargs):
+    if not today:
+        today = date.today()
+    else:
+        today = parse(today).date()
+    r = today + relativedelta(**kwargs)
     return r
 
 
 if __name__ == '__main__':
     # TESTS
-    assert cal_date_incr_from_today(months=2) == parse("11-23-2019").date()
-    assert cal_date_incr_from_today(days=1) == parse("9-24-2019").date()
-    assert cal_date_incr_from_today(days=32) == parse("10-25-2019").date()
-    assert cal_date_incr_from_today(years=1) == parse("9-23-2020").date()
-    assert cal_date_incr_from_today(days=1, years=1, months=1) == parse("10-24-2020").date()
+    assert cal_date_incr_from_today(today="9-23-2019", months=2) == parse("11-23-2019").date()
+    assert cal_date_incr_from_today(today="9-23-2019", days=1) == parse("9-24-2019").date()
+    assert cal_date_incr_from_today(today="9-23-2019", days=32) == parse("10-25-2019").date()
+    assert cal_date_incr_from_today(today="9-23-2019", years=1) == parse("9-23-2020").date()
+    assert cal_date_incr_from_today(today="9-23-2019", days=1, years=1, months=1) == parse("10-24-2020").date()
     print('Tests passed')
