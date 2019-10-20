629855e5880af08b5c34f22899f750a90d6e7d02
Alex shevelo <oshevelo@softserveinc.com>
2019-10-20 10:46:21 +0300

    init web

---
+++ mysite/polls/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.http import Http404
+from polls.models import Question
+
+
+def index(request):
+    question_list = Question.objects.all()
+    return HttpResponse(', '.join([q.question_text for q in question_list]))
+
+
+def index_other(request):
+    return HttpResponse("Hello, OTHER world. You're at the polls index.")
